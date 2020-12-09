c3d9ae98db821ea3d5ddf5a73ce35b918aa41600
LoganSimonsen <logan.simonsen@easypost.com>
2020-12-09 13:53:29 -0700

    day2 part 1 completed

--- day2/script.js
+++ day2/script.js
@@ -1,26 +1,26 @@
-const input = require("./input.js");
+const fs = require('fs');
 
 let totalValid = 0;
 
-function evaluate(){
-    Object.keys(input).forEach(key => {
-        let count = 0;
-        let minMax = key.split(" ");
-        minMax[0] = minMax[0].split("-");
-        let password = input[key].split("");
-        for(i=0;i<password.length;i++){
-            if(password[i] === minMax[1]){
-                count++;
+fs.readFile('./day2/input.txt', 'utf8', (err, lines) => {
+    lines = lines.split(RegExp('\n'))
+    for(i=0;i<lines.length-1;i++){
+        lines[i] = lines[i].split(/[ -.:;?!~,`"&|()<>{}\[\]\r\n/\\]+/)
+        let password = lines[i][3].split("");
+        let letterCount = 0;
+        for(x=0;x<password.length;x++){
+            // console.log(password[x],lines[i][2], password[x]===lines[i][2])
+            if(password[x]===lines[i][2]){
+                letterCount++
             }
         }
-        //==========trying to figure out why my count is wrong...
-        console.log(parseInt(minMax[0][0]), parseInt(minMax[0][1]), count, count>=parseInt(minMax[0][0]) && count<=parseInt(minMax[0][1]));
-        //============
-        if(count>=parseInt(minMax[0][0]) && count<=parseInt(minMax[0][1])){
+        // console.log(letterCount, letterCount<=lines[i][0] && letterCount>=lines[i][1])
+        if(letterCount>=lines[i][0] && letterCount<=lines[i][1]){
             totalValid++
         }
-      });
-      return `there are ${totalValid} valid passwords out of ${Object.keys(input).length}`
-}
+    }
+    console.log(totalValid);
+})
 
-console.log(evaluate())
+
+
