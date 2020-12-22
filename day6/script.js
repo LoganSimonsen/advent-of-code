811ce0959116733d8226a53aead8a9650f715184
LoganSimonsen <logan.simonsen@easypost.com>
2020-12-22 13:04:09 -0700

    day6 part 1 completed

--- day6/script.js
+++ day6/script.js
@@ -1,5 +1,24 @@
 const fs = require('fs');
 
-const lines = fs.readFileSync("./day6/input.txt", {encoding: 'utf-8'})
+const lines = fs.readFileSync("./day6/input.txt", {encoding: 'utf-8'}).split('\n\n').filter(x => x)
+let count = 0;
 
-console.log(lines)
+for(i=0;i<lines.length;i++){
+    lines[i] = lines[i].split("\n").join("")
+    
+}
+
+function removeDuplicateCharacters(string) {
+    return string
+      .split('')
+      .filter(function(item, pos, self) {
+        return self.indexOf(item) == pos;
+      })
+      .join('');
+  }
+  
+  for(i=0;i<lines.length;i++){
+      lines[i] = removeDuplicateCharacters(lines[i]);
+      count+=lines[i].length;
+  }
+  console.log(count)
