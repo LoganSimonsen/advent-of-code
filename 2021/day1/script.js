66474aaef67321e818bf95e47135136ca9d21d36
LoganSimonsen <logan.simonsen@easypost.com>
2021-12-05 22:17:51 -0700

    finished day1, part 2

--- 2021/day1/script.js
+++ 2021/day1/script.js
@@ -8,6 +8,8 @@ const input = fs
 let previousLine = 0;
 let increased = 0;
 let decreased = 0;
+let windowIncrease = 0;
+let windowDecrease = 0;
 
 for (i = 0; i < input.length; i++) {
   if (input[i] > previousLine) {
@@ -16,6 +18,17 @@ for (i = 0; i < input.length; i++) {
     decreased++;
   }
   previousLine = input[i];
+
+  //part2
+
+  let A = (+input[i] || 0) + (+input[i + 1] || 0) + (+input[i + 2] || 0);
+  let B = (+input[i + 1] || 0) + (+input[i + 2] || 0) + (+input[i + 3] || 0);
+  if (B > A) {
+    windowIncrease++;
+  } else {
+    windowDecrease++;
+  }
+  //   console.log(A, B, B > A, windowIncrease);
 }
 
-console.log(increased, decreased);
+console.log(increased, windowIncrease);
